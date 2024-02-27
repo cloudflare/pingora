5ba2494f92aa8459643103b32ffaf9c26e419425
Matthew Gumport <m@gum.pt>
2024-02-27 12:40:58 -0800

    fix warning

--- pingora-limits/src/lib.rs
+++ pingora-limits/src/lib.rs
@@ -24,11 +24,9 @@ pub mod inflight;
 pub mod rate;
 
 use ahash::RandomState;
-use std::hash::{BuildHasher, Hash, Hasher};
+use std::hash::Hash;
 
 #[inline]
 fn hash<T: Hash>(key: T, hasher: &RandomState) -> u64 {
-    let mut hasher = hasher.build_hasher();
-    key.hash(&mut hasher);
-    hasher.finish()
+    hasher.hash_one(&key)
 }
