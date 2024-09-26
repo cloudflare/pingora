de26f4b80234e42f9c811d9521fd626e2e7bc983
Kevin Guthrie <kevinbartlett@cloudflare.com>
2024-09-26 16:49:09 -0400

    Add a warning log if the dictionary for `HeaderSerde` is `None`

--- pingora-cache/src/meta.rs
+++ pingora-cache/src/meta.rs
@@ -595,8 +595,19 @@ fn load_file(path: &String) -> Option<Vec<u8>> {
 }
 
 static HEADER_SERDE: Lazy<HeaderSerde> = Lazy::new(|| {
-    let dict = COMPRESSION_DICT_PATH.get().and_then(load_file);
-    HeaderSerde::new(dict)
+    let dict_path_opt = COMPRESSION_DICT_PATH.get();
+
+    if dict_path_opt.is_none() {
+        warn!("COMPRESSION_DICT_PATH is not set");
+    }
+
+    let result = dict_path_opt.and_then(load_file);
+
+    if result.is_none() {
+        warn!("HeaderSerde not loaded from file");
+    }
+
+    HeaderSerde::new(result)
 });
 
 pub(crate) fn header_serialize(header: &ResponseHeader) -> Result<Vec<u8>> {
