e89f198243b0b375383eacff7d2eed48d019c398
Edward Wang <ewang@cloudflare.com>
2024-11-04 15:08:56 -0800

    Allow errors from upstream_response_body_filter

--- pingora-proxy/src/proxy_trait.rs
+++ pingora-proxy/src/proxy_trait.rs
@@ -267,7 +267,8 @@ pub trait ProxyHttp {
         _body: &mut Option<Bytes>,
         _end_of_stream: bool,
         _ctx: &mut Self::CTX,
-    ) {
+    ) -> Result<()> {
+        Ok(())
     }
 
     /// Similar to [Self::upstream_response_filter()] but for response trailers
