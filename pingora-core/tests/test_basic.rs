cb93a55a6db20f361c7f8a1588a15247d761052d
Harald Gutmann <harald@gutmann.one>
2025-01-15 14:49:05 +0100

    add Quic/Http3 test using quiche/h3i

--- pingora-core/tests/test_basic.rs
+++ pingora-core/tests/test_basic.rs
@@ -14,8 +14,14 @@
 
 mod utils;
 
+use std::env;
+use h3i::actions::h3::send_headers_frame;
+use h3i::frame::H3iFrame;
 #[cfg(all(unix, feature = "any_tls"))]
 use hyperlocal::{UnixClientExt, Uri};
+use log::{debug, error};
+use zstd::zstd_safe::WriteBuf;
+use pingora_error::{ErrorType, OrErr, Result};
 
 #[tokio::test]
 async fn test_http() {
@@ -62,12 +68,96 @@ async fn test_uds() {
 }
 
 #[tokio::test]
-async fn test_udp() {
+async fn test_quic_http3() -> Result<()> {
     use log::info;
     use std::time::Duration;
+    use h3i::actions::h3::Action;
+    use h3i::actions::h3::StreamEvent;
+    use h3i::actions::h3::StreamEventType;
+    use h3i::actions::h3::WaitType;
+    use h3i::client::sync_client;
+    use h3i::config::Config;
+    use h3i::quiche::h3::frame::Frame;
+    use h3i::quiche::h3::Header;
+    use h3i::quiche::h3::NameValue;
 
     utils::init();
-
     info!("Startup completed..");
-    tokio::time::sleep(Duration::from_secs(3600)).await;
+
+    let config = Config::new()
+        .with_connect_to("127.0.0.1:6147".to_string())
+        .with_host_port("openrusty.org:6147".to_string())
+        .with_idle_timeout(2000)
+        .verify_peer(false)
+        .build()
+        .unwrap();
+
+    let body = b"test".to_vec();
+    let headers = vec![
+        Header::new(b":method", b"POST"),
+        Header::new(b":scheme", b"https"),
+        Header::new(b":authority", b"openrusty.org"),
+        Header::new(b":path", b"/"),
+        Header::new(b"content-length", body.len().to_string().as_bytes()),
+    ];
+    const STREAM_ID: u64 = 0;
+    let actions = vec![
+        send_headers_frame(STREAM_ID, false, headers),
+        Action::SendFrame {
+            stream_id: STREAM_ID,
+            fin_stream: true,
+            frame: Frame::Data {
+                payload: body.clone(),
+            },
+        },
+        Action::Wait {
+            wait_type: WaitType::StreamEvent(StreamEvent {
+                stream_id: STREAM_ID,
+                event_type: StreamEventType::Finished,
+            }),
+        },
+        Action::ConnectionClose {
+            error: quiche::ConnectionError {
+                is_app: true,
+                error_code: quiche::h3::WireErrorCode::NoError as u64,
+                reason: vec![],
+            },
+        },
+    ];
+
+    let summary = sync_client::connect(config, &actions, None)
+        .explain_err(ErrorType::H3Error, |e| format!("connection failed {:?}", e))?;
+
+    debug!("summary: {:?}", &summary);
+
+    let stream = summary.stream_map.stream(STREAM_ID);
+    let resp_headers = stream.iter().find(|e| matches!(e, H3iFrame::Headers(..))).unwrap();
+    let resp_body : Vec<Vec<u8>> = stream.iter()
+        .filter_map(|f| {
+            match f {
+                H3iFrame::QuicheH3(f) => {
+                    match f {
+                        Frame::Data { payload } => {
+                            Some(payload.clone())
+                        }
+                        _ => None
+                    }
+                }
+                _ => None
+            }
+        }).collect();
+
+
+    debug!("response headers: {:?}, body: {:?} ", &resp_headers, String::from_utf8(resp_body[0].clone()));
+    let headers = resp_headers.to_enriched_headers().unwrap();
+    let headers = headers.header_map();
+    let status = headers.get(b":status".as_slice()).unwrap();
+    let content_type = headers.get(b"content-type".as_slice()).unwrap();
+    let content_length = headers.get(b"content-length".as_slice()).unwrap();
+    assert_eq!(status, &b"200".to_vec());
+    assert_eq!(content_type, &b"text/html".to_vec());
+    assert_eq!(content_length, &body.len().to_string().as_bytes().to_vec());
+    assert_eq!(resp_body[0], body.as_slice().to_vec());
+
+    Ok(())
 }
