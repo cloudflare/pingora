b9082eb9ecde625f879b5a6adace055f5440989a
Edward Wang <ewang@cloudflare.com>
2025-06-17 23:09:15 -0700

    Add brief sleep to shutdown signal tests to avoid flake
    The signal handlers are installed after the Running phase message has
    been sent, so these tests may be prone to unhandled signals otherwise.
    
    Also adds copyright notice to new tests.

--- pingora-core/tests/server_phase_gracefulshutdown.rs
+++ pingora-core/tests/server_phase_gracefulshutdown.rs
@@ -1,3 +1,17 @@
+// Copyright 2025 Cloudflare, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // NOTE: This test sends a shutdown signal to itself,
 // so it needs to be in an isolated test to prevent concurrency.
 
@@ -36,6 +50,11 @@ fn test_server_execution_phase_monitor_graceful_shutdown() {
 
     // Need to wait for startup, otherwise the signal handler is not
     // installed yet.
+    //
+    // TODO: signal handlers are installed after Running phase
+    // message is sent, sleep for now to avoid test flake
+    std::thread::sleep(std::time::Duration::from_millis(500));
+
     unsafe {
         libc::raise(libc::SIGTERM);
     }
