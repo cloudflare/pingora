a2cf6273a006c71c34a05611597c9e3f502d70bf
Fei Deng <fdeng@cloudflare.com>
2025-11-07 17:12:51 -0500

    fix

--- pingora-boringssl/src/boring_tokio.rs
+++ pingora-boringssl/src/boring_tokio.rs
@@ -263,9 +263,7 @@ where
                 return Poll::Pending;
             }
             Err(e) => {
-                return Poll::Ready(Err(e
-                    .into_io_error()
-                    .unwrap_or_else(io::Error::other)));
+                return Poll::Ready(Err(e.into_io_error().unwrap_or_else(io::Error::other)));
             }
         }
 
