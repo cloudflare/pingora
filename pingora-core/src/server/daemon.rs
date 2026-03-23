10041faed5420476bca9ff3f4ebc657e4e9253f3
Alexander Kjäll <alexander.kjall@gmail.com>
2026-03-23 18:41:00 +0100

    replace daemonize with daemonix, as it's more maintained

--- pingora-core/src/server/daemon.rs
+++ pingora-core/src/server/daemon.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use daemonize::{Daemonize, Stdio};
+use daemonix::{Daemonize, Stdio};
 use log::{debug, error};
 use std::ffi::CString;
 use std::fs::{self, OpenOptions};
